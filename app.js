2f2cc2bca22d4ce2293a15f155b95b544ad50d47
la1314 <44264388+la1314@users.noreply.github.com>
2020-04-13 19:41:15 +0300

    Modificadas consultas

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3069;
 
+// Paginas publicas (estaticas)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded())
 // Utilizaremos body-parser para "parsear lo que nos pidan"
-app.use(bodyParser.urlencoded({
+/*app.use(bodyParser.urlencoded({
     extended: true
-}));
+}));*/
 
 //Parsearemos los jsones
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 
 // Vamos a definir un "punto de inicio"
@@ -18,12 +22,8 @@ app.get('/api/', (req, res) => {
     res.json({ "message": "API de mcnreader" });
 });
 
-// Paginas publicas (estaticas)
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 // Require Puntuaciones routes
-require('./app/routes/puntuaciones.routes.js')(app);
+require('./app/routes/db.routes.js')(app);
 
 // Escuchemos en un puerto
 app.listen(port, () => console.log(`* [ mcnreader ] UP and Running en http://localhost: ${port}`));
